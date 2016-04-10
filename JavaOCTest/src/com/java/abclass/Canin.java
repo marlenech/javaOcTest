b3f86c21abd3b78c1d7f4f73dc1cdf82f9f35284
marlenech <italikdesignbordeaux@gmail.com>
2016-04-10 17:04:08 +0200

    Create abstract class for Canin that inherits Animal's class

---
+++ JavaOCTest/src/com/java/abclass/Canin.java
@@ -0,0 +1,11 @@
+package com.java.abclass;
+
+//Create abstract class for Canin that inherits Animal's class
+
+public abstract class Canin extends Animal {
+	void deplacement(){
+		System.out.println("je me déplace en meute");
+		
+	}
+
+}
