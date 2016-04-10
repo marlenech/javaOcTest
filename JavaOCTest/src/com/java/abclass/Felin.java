ede03ed9611bb565862ec52a2198e5098a91d3ef
marlenech <italikdesignbordeaux@gmail.com>
2016-04-10 17:01:25 +0200

    Create abstract class for Felin that inherits Animal's class

---
+++ JavaOCTest/src/com/java/abclass/Felin.java
@@ -0,0 +1,11 @@
+package com.java.abclass;
+
+//Create abstract class for Felin that inherits Animal's class
+
+public abstract class Felin extends Animal{
+	
+	void deplacement(){
+		System.out.println("Je me déplace seul");
+	}
+
+}
