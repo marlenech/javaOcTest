de0cbc4ec2ed919d6b7a9c1b312ed7a9dfc3a0c4
marlenech <italikdesignbordeaux@gmail.com>
2016-04-21 09:59:34 +0200

    Create Table with double dimension and display the third given in Table 2

---
+++ JavaOCTest/src/com/java/Tableau/Tableau.java
@@ -0,0 +1,15 @@
+package com.java.Tableau;
+
+public class Tableau {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Create table with double dimension
+		
+		int tableau [] [] = {{1,3,5,9},{0,2,4,6}};
+		
+		System.out.println(tableau [1] [2]);
+	}
+
+}
