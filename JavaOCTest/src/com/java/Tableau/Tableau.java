32fed50396cfee0352064883922245024bb1da5e
marlenech <italikdesignbordeaux@gmail.com>
2016-04-21 16:54:39 +0200

    Display all the data tables of "tab" Display 3rd data of second table "tableau" Display all the data tables of "tableau"

--- JavaOCTest/src/com/java/Tableau/Tableau.java
+++ JavaOCTest/src/com/java/Tableau/Tableau.java
@@ -7,9 +7,34 @@ public class Tableau {
 
 		// Create table with double dimension
 		
-		int tableau [] [] = {{1,3,5,9},{0,2,4,6}};
+		String tableau [] [] = {{"1","3","5","9"},{"0","2","4","6"}};
 		
+		int [] tab  = {10, 11, 12};
+		
+		for (int i = 0; i<=tab.length; i++);
+		{
+		
+		// Display all the data tables of "tab"
+		System.out.println(java.util.Arrays.toString(tab));
+		}
+		// Display 3rd data of second table "tableau"		
 		System.out.println(tableau [1] [2]);
+		
+		// Display all the data tables of "tableau"
+		
+		for (String tab1[] : tableau) {
+			for (String str : tab1) 
+		{
+		System.out.println(str);
+		}
+			}
+		
 	}
+		
+		
+		
+			}
+		
+	
 
-}
+
